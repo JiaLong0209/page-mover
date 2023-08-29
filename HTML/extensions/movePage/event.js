6d2ced2cb7961978fcade84c3cf7f326d0bd148e
Jialong0209 <smt02091395@gmail.com>
2023-08-29 23:44:40 +0800

    230829 add commands key

--- HTML/extensions/movePage/event.js
+++ HTML/extensions/movePage/event.js
@@ -1,4 +1,14 @@
+let active = true
 
+ 
 chrome.runtime.onInstalled.addListener(async () => {
-    console.log('Extension is Installed');
-  });
+  console.log('Extension is Installed');
+});
+
+chrome.commands.onCommand.addListener(function (command) {
+  console.log('Command:', command);
+  if(command == 'toggle_move_page'){
+    active = !active;
+    console.log(`${active ? 'active' : 'non-active'}`)
+  }
+});
