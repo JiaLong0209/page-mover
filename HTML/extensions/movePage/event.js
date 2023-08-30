6e78b8427621f23f91d2acccbfb4e9334360d6f9
Jialong0209 <smt02091395@gmail.com>
2023-08-30 22:56:36 +0800

    230830 v0.7.0 add active move page to contexxt menus

--- HTML/extensions/movePage/event.js
+++ HTML/extensions/movePage/event.js
@@ -1,14 +1,68 @@
 let active = true
 
- 
+function toggleMovePageFunction(status) {
+  active = status;
+  console.log(`${active ? 'active' : 'non-active'}`)
+}
+
+function genericOnClick(info, tab) {
+  //根據你點選右鍵的狀況不同，可以得到一些跟內容有關的資訊  
+  //例如 頁面網址，選取的文字，圖片來源，連結的位址  
+  let id = info.menuItemId;
+  console.log(
+    "Info :" + JSON.stringify(info) + "\n" +
+    "ID是:" + info.menuItemId + "\n" +
+    "現在的網址是:" + info.pageUrl + "\n" +
+    "選取的文字是:" + (info.selectionText ? info.selectionText : "") + "\n" +
+    "現在hover元素的圖片來源:" + (info.srcUrl ? info.srcUrl : "") + "\n" +
+    "現在hover的連結:" + (info.linkUrl ? info.linkUrl : "") + "\n" +
+    "現在hover的frame是:" + (info.frameUrl ? info.frameUrl : "") + "\n"
+  );
+  if(id == "toggleMovePage"){
+    toggleMovePageFunction(info.checked)
+    
+  }
+
+}
+
 chrome.runtime.onInstalled.addListener(async () => {
   console.log('Extension is Installed');
+  let parent = chrome.contextMenus.create({
+    "title": "All functions",
+    "contexts": ['all'],
+    "id": "parent"
+  })
+
+  let toggleMovePage = chrome.contextMenus.create({
+    "id": "toggleMovePage",
+    "title": "active move page",
+    "type": "checkbox",
+    "contexts": ['all'],
+    "parentId": parent,
+    "checked": true 
+  });
+
+  chrome.commands.onCommand.addListener(function (command) {
+    console.log('Command:', command);
+    command == 'toggle_move_page' ? toggleMovePageFunction(!active) : null;
+    chrome.contextMenus.update("toggleMovePage", {  // update the checkBox
+      "checked": active
+    });
+  });
+
+  chrome.contextMenus.onClicked.addListener(genericOnClick);
+
 });
 
-chrome.commands.onCommand.addListener(function (command) {
-  console.log('Command:', command);
-  if(command == 'toggle_move_page'){
-    active = !active;
-    console.log(`${active ? 'active' : 'non-active'}`)
-  }
-});
+// {  
+//   "type" : "normal",  
+//   "id" : "item1-1",  
+//   "title" : "使用者選擇了'%s'",  
+//   "contexts" : ["all", "page", "frame", "selection", "link", "editable", "image", "video", "audio", "launcher", "browser_action","page_action"],  
+//   "documentUrlPatterns" : ["https://*.google.com/foo*bar"],  
+//   "targetUrlPatterns" : [],  
+//   "enabled": true,  
+//   "onclick": function(info,tab){},  
+//   "parentId": "item1",  
+//   "checked" : false  
+// };
