b241ac837ba055cb3b8853695a4be9dc1716c573
Jialong0209 <smt02091395@gmail.com>
2023-09-04 16:56:40 +0800

    230904 v0.9.3 adjust scrolling related parameters and optimize the scrolling distance offset

--- HTML/extensions/movePage/content.js
+++ HTML/extensions/movePage/content.js
@@ -1,31 +1,34 @@
 /*      DevLogs
-    230821 v0.3.0   add key "hjklwasdmMigG" move page
-    230822 v0.3.1   remove unnecessary code in content.js
-    230823 v0.3.2   fix minor bug in content.js 
-    230824 v0.4.0   optimize movePageByKey UI
-    230824 v0.4.1   rename movePageByKey to movePage
-    230824 v0.4.2   change favicon
-    230824 v0.4.3   add background script
-    230828 v0.4.4   add github link
-    230828 v0.5.0   change open link method (create tab)
-    230828 v0.5.1   optimize the github link UI
-    230829 v0.6.0   add commands key
-    230830 v0.7.0   add active move page to context menus
-    230831 v0.7.1   remove necessary code in content.js
-    230831 v0.8.0   add continuous scroll mode 
-    230901 v0.8.1   fix move page active in input area 
-    230901 v0.8.2   fix up/down moving distance difference 
-    230901 v0.8.3   change argument 'e.key' to 'e' in scrollByKey function
-    230901 v0.8.4   fix move page not work in continuous scroll mode
-    230904 v0.9.0   add open new tab shortcut "Alt+N" and a context menu button
-    230904 v0.9.1   add 'nonlinear' and 'linear' scroll mode
-    230904 v0.9.2   rename 'times', 'count' , 'midCount' to 'step', 'totalSteps', 'midSteps' in content.js
+    230821 v0.3.0   Add key "hjklwasdmMigG" move page
+    230822 v0.3.1   Remove unnecessary code in content.js
+    230823 v0.3.2   Fix minor bug in content.js 
+    230824 v0.4.0   Optimize movePageByKey UI
+    230824 v0.4.1   Rename movePageByKey to movePage
+    230824 v0.4.2   Change favicon
+    230824 v0.4.3   Add background script
+    230828 v0.4.4   Add github link
+    230828 v0.5.0   Change open link method (create tab)
+    230828 v0.5.1   Optimize the github link UI
+    230829 v0.6.0   Add commands key
+    230830 v0.7.0   Add active move page to context menus
+    230831 v0.7.1   Remove necessary code in content.js
+    230831 v0.8.0   Add continuous scroll mode 
+    230901 v0.8.1   Fix move page active in input area 
+    230901 v0.8.2   Fix up/down moving distance difference 
+    230901 v0.8.3   Change argument 'e.key' to 'e' in scrollByKey function
+    230901 v0.8.4   Fix move page not work in continuous scroll mode
+    230904 v0.9.0   Add open new tab shortcut "Alt+N" and a context menu button
+    230904 v0.9.1   Add 'nonlinear' and 'linear' scroll mode
+    230904 v0.9.2   Rename 'times', 'count' , 'midCount' to 'step', 'totalSteps', 'midSteps' in content.js
+    230904 v0.9.3   Adjust scrolling related parameters and optimize the scrolling distance offset
 
         TODO:
     Github link                  v 230828
     continuous mode              v 230831
+    
     *active button
     *toggle scroll mode 
+    *bug in continuous mode when pressing Shift key
 
         Note:
     git commit --amend -m "commit message" // can amend last commit message
@@ -37,9 +40,11 @@ let interval = 1000 / 120; // 120 FPS
 let minSpeed = 0, maxSpeed = 2;
 let power = 1.2;
 let activeMode = 0; // 0 = non-continuous, 1 = continuous
-let scrollMode = 'linear' // nonlinear, nonlinear_power or linear
+let scrollMode = 'nonlinear_power' // nonlinear, nonlinear_power or linear
 let pressedKeys = {}
-let xOffset = 10;
+let contDistOffset = 6;
+let contDurationOffset = 3;
+let xOffset = 10 * (contDistOffset / 3) / contDurationOffset;   // fix the moving distance difference when move down/up
 
 function scrollByDistance(x, y, duration) {
 
@@ -107,15 +112,15 @@ function scrollByKey(e, distance, duration) {
             break;
 
         case 'g':   // scroll to top
-            scrollByDistance(0, -window.scrollY, duration * 3);
+            scrollByDistance(0, -window.scrollY, duration * 5);
             break;
 
         case 'G':   // scroll to bottom
-            scrollByDistance(0, document.body.scrollHeight - window.scrollY - window.innerHeight + 150, duration * 3);
+            scrollByDistance(0, document.body.scrollHeight - window.scrollY - window.innerHeight + 150, duration * 5);
             break;
 
         case 'M':   // scroll to middle
-            scrollByDistance(0, (document.body.scrollHeight / 2 - window.scrollY - window.screen.height / 2), duration * 2);
+            scrollByDistance(0, (document.body.scrollHeight / 2 - window.scrollY - window.screen.height / 2), duration * 3);
             break;
 
         case ' ':
@@ -147,7 +152,7 @@ window.addEventListener('keyup', (e) => { pressedKeys[e.key] = false })
 setInterval(() => {
     if (!isInputArea(document.activeElement.tagName) && activeMode == 1) {
         for (let [key, value] of Object.entries(pressedKeys)) {
-            if (value) scrollByKey(new KeyboardEvent('keydown', { key: key }), scrollDist / 6, scrollTime / 2);
+            if (value) scrollByKey(new KeyboardEvent('keydown', { key: key }), scrollDist / contDistOffset, scrollTime / contDurationOffset);
         }
     }
 }, 33);
